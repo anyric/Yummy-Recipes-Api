75c5bdcb715cfae3edd7cc58c6f5ff5ce20e74b7
Anyama Richard <richard@Anyamas-MacBook-Pro.local>
2018-01-06 13:41:11 +0300

    imported heroku and updated requirements.txt file

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
+from flask-heroku import heroku
 from flasgger import Swagger
 from apps import config
 
@@ -21,7 +22,7 @@ app.config.from_object(config_name)
 app.config['PAGINATION_PAGE_SIZE']=2
 app.config['PAGINATION_PAGE_ARGUMENT_NAME']='page'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+heroku = Heroku(app)
 auth = HTTPBasicAuth()
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
